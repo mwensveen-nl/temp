425d57e6a9ffb3087e6cad530338b22be0d69d1a
Micha Wensveen <mail@mwensveen.nl>
2024-05-03 15:08:30 +0200

    test

--- src/main/java/nl/mwensveen/temp/Main.java
+++ src/main/java/nl/mwensveen/temp/Main.java
@@ -1,9 +1,13 @@
 package nl.mwensveen.temp;
 
+import io.jsondb.JsonDBTemplate;
+
 public class Main {
+    private static JsonDBTemplate jsonDBTemplate;
 
     public static void main(String[] args) {
         System.out.println("Temp");
+        jsonDBTemplate = new JsonDBTemplate(".", ".");
     }
 
 }
